d8708b073c42d3642d446c6a56a89067ff3c4220
yor42 <yoonch1022@naver.com>
2021-05-30 00:43:42 +0900

    fixed duplicated starting item

--- src/main/java/com/yor42/projectazure/events/ModBusEventHandler.java
+++ src/main/java/com/yor42/projectazure/events/ModBusEventHandler.java
@@ -46,16 +46,16 @@ public class ModBusEventHandler {
             boolean flag = !data.getBoolean("PRJA:gotStarterCube");
 
             if (flag) {
-                    PlayerEntity player = event.getPlayer();
-                    UUID yorUUID = UUID.fromString("d45160dc-ae0b-4f7c-b44a-b535a48182d2");
-                    boolean isDev = player.getUniqueID().equals(yorUUID);
-                    player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.Rainbow_Wisdom_Cube.get()));
-                    if (isDev) {
-                        player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWM_AYANAMI.get()));
-                        player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWM_ENTERPRISE.get()));
-                        player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWN_GANGWON.get()));
-                    }
-                data.putBoolean(defined.MODID + ":gotStarterCube", true);
+                PlayerEntity player = event.getPlayer();
+                UUID yorUUID = UUID.fromString("d45160dc-ae0b-4f7c-b44a-b535a48182d2");
+                boolean isDev = player.getUniqueID().equals(yorUUID);
+                player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.Rainbow_Wisdom_Cube.get()));
+                if (isDev) {
+                    player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWM_AYANAMI.get()));
+                    player.inventory.setInvenxtorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWM_ENTERPRISE.get()));
+                    player.inventory.setInventorySlotContents(player.inventory.getFirstEmptyStack(), new ItemStack(registerItems.SPAWN_GANGWON.get()));
+                }
+                data.putBoolean("PRJA:gotStarterCube", true);
                 playerData.put(PlayerEntity.PERSISTED_NBT_TAG, data);
             }
         }
