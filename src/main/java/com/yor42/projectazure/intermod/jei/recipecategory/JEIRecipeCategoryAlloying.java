06667fd6fe548f4a9c91bfe7ee6b5e0e36122bb6
yor42 <yoonch1022@naver.com>
2022-04-28 01:26:17 +0900

    Attempt to fix JEI plugin

--- src/main/java/com/yor42/projectazure/intermod/jei/recipecategory/JEIRecipeCategoryAlloying.java
+++ src/main/java/com/yor42/projectazure/intermod/jei/recipecategory/JEIRecipeCategoryAlloying.java
@@ -3,15 +3,21 @@ package com.yor42.projectazure.intermod.jei.recipecategory;
 import com.yor42.projectazure.gameobject.crafting.AlloyingRecipe;
 import com.yor42.projectazure.libs.utils.ResourceUtils;
 import com.yor42.projectazure.setup.register.registerBlocks;
-import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+
+import static mezz.jei.api.constants.VanillaTypes.ITEM;
 
 public class JEIRecipeCategoryAlloying implements IRecipeCategory<AlloyingRecipe> {
 
@@ -20,7 +26,7 @@ public class JEIRecipeCategoryAlloying implements IRecipeCategory<AlloyingRecipe
     public static final ResourceLocation UID = ResourceUtils.ModResourceLocation("jei_alloying");
 
     public JEIRecipeCategoryAlloying(IGuiHelper guiHelper) {
-        this.icon = guiHelper.createDrawableIngredient(new ItemStack(registerBlocks.ALLOY_FURNACE.get().asItem()));
+        this.icon = guiHelper.createDrawableIngredient(ITEM, new ItemStack(registerBlocks.ALLOY_FURNACE.get().asItem()));
 
         ResourceLocation TEXTURE = ResourceUtils.ModResourceLocation("textures/gui/alloy_furnace.png");
         this.background = guiHelper.createDrawable(TEXTURE, 4,4, 168, 75);
@@ -38,8 +44,8 @@ public class JEIRecipeCategoryAlloying implements IRecipeCategory<AlloyingRecipe
     }
 
     @Override
-    public String getTitle() {
-        return "recipe.alloying";
+    public Component getTitle() {
+        return new TranslatableComponent("recipe.alloying");
     }
 
     @Override
@@ -53,9 +59,14 @@ public class JEIRecipeCategoryAlloying implements IRecipeCategory<AlloyingRecipe
     }
 
     @Override
+    public void setRecipe(IRecipeLayoutBuilder builder, AlloyingRecipe recipe, IFocusGroup focuses) {
+        IRecipeCategory.super.setRecipe(builder, recipe, focuses);
+    }
+
+    @Override
     public void setIngredients(AlloyingRecipe alloyingRecipe, IIngredients iIngredients) {
-        iIngredients.setInputs(VanillaTypes.ITEM, alloyingRecipe.getIngredientStack());
-        iIngredients.setOutput(VanillaTypes.ITEM, alloyingRecipe.getResultItem());
+        iIngredients.setInputs(ITEM, alloyingRecipe.getIngredientStack());
+        iIngredients.setOutput(ITEM, alloyingRecipe.getResultItem());
     }
 
     @Override
