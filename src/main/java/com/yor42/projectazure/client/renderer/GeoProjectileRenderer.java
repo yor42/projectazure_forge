42f6e184a430033c49b85b195081bf14f3d6f91e
yor42 <yoonch1022@naver.com>
2022-05-25 16:59:03 +0900

    messing with multiblocked

--- src/main/java/com/yor42/projectazure/client/renderer/GeoProjectileRenderer.java
+++ src/main/java/com/yor42/projectazure/client/renderer/GeoProjectileRenderer.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.vector.Vector3f;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.IAnimatableModel;
 import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.util.Color;
 import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.model.provider.GeoModelProvider;
