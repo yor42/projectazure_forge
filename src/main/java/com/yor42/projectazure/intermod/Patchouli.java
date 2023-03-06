7ed04dcbb2731bb1cc7cb56e2a62178247c4f6d4
yor42 (Steam Deck) <yoonch1022@navercom)>
2023-03-06 10:52:18 +0900

    add missing translation

--- src/main/java/com/yor42/projectazure/intermod/Patchouli.java
+++ src/main/java/com/yor42/projectazure/intermod/Patchouli.java
@@ -7,7 +7,10 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.UUID;
@@ -18,7 +21,7 @@ public class Patchouli {
         PlayerInventory inv = player.inventory;
 
         if(!CompatibilityUtils.isPatchouliLoaded()) {
-            player.sendMessage(new TranslationTextComponent("message.nopatchouliinstalled"), UUID.randomUUID());
+            player.sendMessage(new TranslationTextComponent("message.nopatchouliinstalled", new TranslationTextComponent("modnames.patchouli").withStyle(TextFormatting.AQUA).setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://curseforge.com/minecraft/search?page=1&&pageSize=20&sortType=2&class=mc-mods")))).withStyle(TextFormatting.GRAY), UUID.randomUUID());
         }
 
         Item book = ForgeRegistries.ITEMS.getValue(new ResourceLocation("patchouli:guide_book"));
