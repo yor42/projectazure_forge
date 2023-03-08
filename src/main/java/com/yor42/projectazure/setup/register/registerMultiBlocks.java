e0ee671a628f80281e654c185f6343b513bb5766
yor42 <49832212+yor42@users.noreply.github.com>
2023-03-09 01:17:09 +0900

    ㅁㄴㅇㄹ

--- src/main/java/com/yor42/projectazure/setup/register/registerMultiBlocks.java
+++ src/main/java/com/yor42/projectazure/setup/register/registerMultiBlocks.java
@@ -1,16 +1,22 @@
 package com.yor42.projectazure.setup.register;
 
 import com.lowdragmc.multiblocked.Multiblocked;
+import com.lowdragmc.multiblocked.api.definition.ControllerDefinition;
+import com.lowdragmc.multiblocked.api.definition.PartDefinition;
+import com.lowdragmc.multiblocked.api.registry.MbdComponents;
 import com.yor42.projectazure.gameobject.blocks.tileentity.multiblock.*;
 import com.yor42.projectazure.gameobject.blocks.tileentity.multiblock.hatches.HatchTE;
 import com.yor42.projectazure.libs.Constants;
+import com.yor42.projectazure.libs.utils.ResourceUtils;
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import javax.swing.plaf.multi.MultiButtonUI;
+import java.util.function.BiConsumer;
 
 @Mod.EventBusSubscriber(modid = Constants.MODID, bus=Mod.EventBusSubscriber.Bus.MOD)
 public class registerMultiBlocks {
@@ -22,6 +28,7 @@ public class registerMultiBlocks {
         AmmoPressControllerTE.registerTE();
         RiftwayControllerTE.registerTE();
         AdvancedAlloySmelterControllerTE.registerTE();
+        MbdComponents.registerComponentFromResource(Multiblocked.GSON, ResourceUtils.ModResourceLocation("controller/projectazure_silicone_crucible"), ControllerDefinition::new, null);
     }
 
     public static void register(){}
